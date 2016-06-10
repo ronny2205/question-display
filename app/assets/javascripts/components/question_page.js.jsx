e8162e6704c663a0766773fa0ded21435501427f
Ronny Almog <ronny2205@gmail.com>
2016-06-10 18:53:19 +0000

    creating the QuestionPage component

---
+++ app/assets/javascripts/components/question_page.js.jsx
@@ -0,0 +1,17 @@
+// The top component
+var QuestionPage = React.createClass({
+
+  render: function() {
+    return (
+      <div className="">
+        
+    	  <h3>Questions</h3>
+    	
+    	
+    	  
+    	
+      </div>
+
+    );
+  }
+});
